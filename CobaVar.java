6a5a340840e9e8d02d0ca298206b50fb81b073cb
FARAH210 <90201315+FARAH210@users.noreply.github.com>
2021-09-15 12:26:09 +0700

    Add files via upload

---
+++ CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaDepan = "Farah";
+        int umur = 18;
+        double tinggiBadan = 153;
+        System.out.println("Nama Saya " + namaDepan);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+    }    
+}
---
+++ Hello.java
@@ -0,0 +1,10 @@
+/*
+NIM:21537144036
+Nama:Farah
+*/
+public class Hello {
+    public static void main(String[] args) {
+        //untuk menampilkan teks
+        System.out.println("Hello world");
+    }
+}
