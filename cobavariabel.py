972c796a5b4f1ee51a918a4d2d6993b06e2185a8
FARAH210 <90201315+FARAH210@users.noreply.github.com>
2021-09-15 20:43:10 +0700

    commit
    belajar data dipython

---
+++ cobavariabel.py
@@ -0,0 +1,9 @@
+namaDepan : "Farah"
+umur : 18
+tinggiBadan : 153
+print("Nama saya adalah" + namaDepan)
+print("Umur saya adalah" + str(umur) + " tahun")
+print(tinggiBadan)
+print(type(namaDepan))
+print(type(umur))
+print(type(tinggiBadan))
