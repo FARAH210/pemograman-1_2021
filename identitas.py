b2c1a23d2252df4ecdc4b922267ced08e5082ed2
FARAH210 <90201315+FARAH210@users.noreply.github.com>
2021-09-16 20:10:33 +0700

    Tugas pertemuan 3

---
+++ identitas.py
@@ -0,0 +1,21 @@
+nama = "Farah Antun Jandini"
+NIM = 21537144036
+angkatan = 2021
+programStudi = "teknologiInformasi"
+jurusan = "JPTEI"
+fakultas = "teknik"
+ipkharapan = 3.8
+print("Nama saya adalah" + (nama)
+print("NIM saya adalah" + str(NIM))
+print("Angkatan saya adalah" + str(angkatan))
+print("programStudi saya adalah" + programStudi)
+print("Jurusan saya adalah" + jurusan)
+print("Fakultas saya adalah" + fakultas)
+print("ipkharapan saya adalah" + str(ipkharapan))
+print(type(nama))
+print(type(NIM))
+print(type(angkatan))
+print(type(programStudi))
+print(type(jurusan))
+print(type(fakultas))
+print(type(ipkharapan))
